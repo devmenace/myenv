70d1eb6a80d0bbcc58771f12aa5ab4b13b52f554
Deniss <denazebiz@gmail.com>
2019-08-16 21:12:55 +0100

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+const source = 'www/public/';
+
+function server() {
+    browserSync.init({
+        server: {
+            baseDir: './' + source
+        }
+    });
+    gulp.watch(source + '*.html').on('change', browserSync.reload);
+    gulp.watch(source + 'assets/css/*.css').on('change', browserSync.reload);
+    gulp.watch(source + 'assets/js/*.js').on('change', browserSync.reload);
+}
+exports.server = server;
