14b453909d03ff867e9c5fc63403355555f2dc5c
Deniss <denazebiz@gmail.com>
2019-08-17 12:30:16 +0100

    just setting files

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ const browserSync = require('browser-sync').create();
 
 const source = 'www/public/';
 
+const server =
 function server() {
     browserSync.init({
         server: {
             baseDir: source
         }
     });
-}
+};
     gulp.watch(source + '*.html').on('change', browserSync.reload);
     gulp.watch(source + 'assets/css/*.css').on('change', browserSync.reload);
     gulp.watch(source + 'assets/js/*.js').on('change', browserSync.reload);
