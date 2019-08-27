170108811d11e5e8bb148810c85b701a26e3176e
Deniss <denazebiz@gmail.com>
2019-08-27 18:01:00 +0100

    Php dev server on localhost and proxy browser-sync reload on change on localhost:8080

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,44 @@
 const gulp = require('gulp');
+const shell = require('gulp-shell');
 const browserSync = require('browser-sync').create();
-
-const source = 'www/public/';
-
-const server =
-function server() {
-    browserSync.init({
-        server: {
-            baseDir: source
-        }
-    });
+const dir = {
+    src: 'www/site/public/',
+    build: 'www/build/'
 };
-    gulp.watch(source + '*.html').on('change', browserSync.reload);
-    gulp.watch(source + 'assets/css/*.css').on('change', browserSync.reload);
-    gulp.watch(source + 'assets/js/*.js').on('change', browserSync.reload);
-exports.server = server;
+
+
+const php =
+    function php() {
+        return gulp.series(
+            shell.task([
+                'echo STARTING PHP DEV SERVER',
+                'php -S 127.0.0.1:80 -t www/site/public/',
+            ])   //?
+        )();
+    };
+
+// Browser sync server
+const bsp =
+    function bsp() {
+        browserSync.init({
+            port: 8080,
+            proxy: 'localhost',
+            baseDir: 'www/site/public/',
+            ui: {
+                port: 3020
+            }
+        });
+        gulp.watch(dir.src + '**/*.php').on('change', browserSync.reload);
+        gulp.watch(dir.src + 'assets/css/*.css').on('change', browserSync.reload);
+        gulp.watch(dir.src + 'assets/js/*.js').on('change', browserSync.reload);
+    };
+
+gulp.task('default', gulp.parallel(
+    gulp.series(bsp),
+    gulp.series(php)
+));
+
+exports.php = php;
+exports.bsp = bsp;
+exports.default = 'default';
+
